4f54e02dfeb292454d0c00858191a9e62b00aac8
YuGod <caoqiais@163.com>
2017-01-03 18:30:49 +0800

    Initial Commit

---
+++ OpenShop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenShop
+//
+//  Created by yuemin3 on 2017/1/3.
+//  Copyright © 2017年 hangzhou.cao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OpenShop/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OpenShop
+//
+//  Created by yuemin3 on 2017/1/3.
+//  Copyright © 2017年 hangzhou.cao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
